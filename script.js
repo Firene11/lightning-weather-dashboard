d6f7a5280bb75ec43b10931386c2c396921c4204
Fih <124883284+Firene11@users.noreply.github.com>
2023-06-09 19:49:29 -0300

    added query selectors for data componants, set dayjs into function to put togther at later time

--- script.js
+++ script.js
@@ -7,9 +7,6 @@ var lon = "";
 var state = "";
 var country = "";
 
-var today = dayjs().format ("MM-DD-YYYY");
-$(".current-day").html(today);
-
 //https://api.openweathermap.org/data/2.5/weather?id={city id}&appid={API key}
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=6509d1e4a713732bea01a624be400633";
 
@@ -19,40 +16,59 @@ var fiveDayForecastURL = "http://api.openweathermap.org/data/2.5/forecast?q=25.7
 // GEOCODING API -- http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
 var geoCodeURL = "http://api.openweathermap.org/geo/1.0/direct?q=Miami,Florida,USA&limit=8&appid=6509d1e4a713732bea01a624be400633";
 
-var cityOutput = $("#search-output");
-var currentDay = $(".current-day");
-var fiveDayForecast = $("#card");
-var form = $("#city-name");
-var searchBtn = $('#search-button');
-var leftSidebar = $('#left-sidebar');
-var content = $('#content');
 
-//if else statement if bad URL
+var cityEl = document.querySelector(".city-name");
+var icon = document.querySelectorAll(".icon");
+var tempEl = document.querySelectorAll(".temp");
+var windEl = document.querySelectorAll(".wind");
+var humidityEl = document.querySelectorAll(".humidity");
 
-    fetch(queryURL).then(function(response) {
-        if (response.status !== 200) {
-            console.log(response.status);
-        } else {
-            return response.json();
-        }    
-        })
-
-.then(function(data) {
-    console.log(data);
-    this.getWeather(data);
-    })
-
+var today = document.querySelector(".current-day");
+var day1 = document.querySelector("#day-1");
+var day2 = document.querySelector("#day-2");
+var day3 = document.querySelector("#day-3");
+var day4 = document.querySelector("#day-4");
+var day5 = document.querySelector("#day-5");
 
 //pull the details needed from the API's object
 function getWeather(data) {
     var { name } = data;
     //the [0] pulls from the first element of the array
-    var { icon, description } = data.weather[0];
+    var { icon } = data.weather[0];
     var { temp, humidity } = data.main;
     var { speed } = data.wind;
-    console.log(name, icon, description, temp, humidity, speed);
+    console.log(name, icon, temp, humidity, speed);
+    cityEl.innerText = name;
+    icon.src = "https://openweathermap.org/img/wn/" + icon + ".png";
+    tempEl.innerText = "Temp:" + temp + "°F";
+    windEl.innerText = "Wind:" + speed + "MPH";
+    humidityEl.innerText = "Humidity:" + humidity + "%";
 }
 
+//set up dates
+function setDates () {
+var { today, next1, next2, next3, next4, next5 } = dayjs().format ("MM-DD-YYYY");
+$(".current-day").html(today);
+$(".day1").html(next1);
+$(".day2").html(next2);
+$(".day3").html(next3);
+$(".day4").html(next4);
+$(".day5").html(next5);
+}
+
+//if else statement if bad URL
+
+fetch(queryURL).then(function(response) {
+    if (response.status !== 200) {
+        console.log(response.status);
+    } else {
+        return response.json();
+    }    
+    })
+    .then(function(data) {
+    console.log(data);
+    this.getWeather(data);
+    })
 
 
 
