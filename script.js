d34e6aaee26c18e795aaf6c7d13088d608974ea0
Fih <124883284+Firene11@users.noreply.github.com>
2023-06-20 17:57:39 -0300

    reverted

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 var APIKey = "6509d1e4a713732bea01a624be400633";
 
-// five day forecast -- api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
-//var fiveDayForecastURL = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=6509d1e4a713732bea01a624be400633";
-
-//var lat = "";
-//var lon = "";
 var city = "Fredericton";
+var cities = [];
+var lat = 45.9454;
+var lon = -66.6656;
 
-var currentWeather = document.getElementById("current-day");
-var searchBttn = document.getElementById("search-button");
+//https://api.openweathermap.org/data/2.5/weather?id={city id}&appid={API key}
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=6509d1e4a713732bea01a624be400633";
-var userInput = document.getElementById("user-input");
+
+// five day forecast -- api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+var fiveDayForecastURL = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=6509d1e4a713732bea01a624be400633";
+
+// GEOCODING API -- http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+var geoCodeURL = "http://api.openweathermap.org/geo/1.0/direct?q=Miami,Florida,USA&limit=8&appid=6509d1e4a713732bea01a624be400633";
+
+var searchOutput = document.getElementById("#search-output");
+
+var input = document.querySelector("#input-box");
+var searchButton = document.querySelector("#search-button");
+var todaysWeather = document.querySelector(".current-day");
+
+function getWeather(data) {
+
 
 //Fetch request for data from API
 fetch(queryURL)
@@ -19,20 +30,13 @@ fetch(queryURL)
     return response.json();
     if (response.status !== 200) {
         document.location.replace("./404.html")
-    } else if (userInput.length == 0) {
-        result.innerHTML = '<h4 class="msg">The input cannot be empty</h4>';
     } else {
         return response.json();
     }
 })
 .then(function(data) {
     console.log(data);
-    var myCity = data;
-    console.log(myCity.weather[0]);
-    console.log(myCity.main);
-    console.log(myCity.wind);
-
-    //pull data from object
+    //pull data from objext array
     var { name } = data;
     //the [0] pulls from the first element of the array
     var date = dayjs().format ("dddd-MM-DD-YYYY");
@@ -40,7 +44,6 @@ fetch(queryURL)
     var { temp, humidity } = data.main;
     var { speed } = data.wind;
     console.log(name, icon, temp, humidity, speed);
-
     //Display in HTML here//
     document.querySelector(".city-name").innerText = name;
     document.querySelector(".icon-today").src = "https://openweathermap.org/img/wn/" + icon + ".png";
@@ -48,7 +51,6 @@ fetch(queryURL)
     document.querySelector(".wind-today").innerText = "Wind:" + speed + "MPH";
     document.querySelector(".humidity-today").innerText = "Humidity:" + humidity + "%";
     document.querySelector(".date-today").innerText = date;
-
 })
 .catch(function(error) {
     console.log(error);
@@ -78,7 +80,7 @@ fetch(fiveDayForecastURL)
     console.log(icon, temp, humidity, speed);
     //Display in HTML here//
 
-    document.querySelector(".icon1").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".icon-1").src = "https://openweathermap.org/img/wn/" + icon + ".png";
     document.querySelector(".temp1").innerText = "Temp:" + temp + "°F";
     document.querySelector(".wind1").innerText = "Wind:" + speed + "MPH";
     document.querySelector(".humidity1").innerText = "Humidity:" + humidity + "%";
@@ -92,7 +94,7 @@ fetch(fiveDayForecastURL)
     var { speed } = data.list[1].wind;
     console.log(icon, temp, humidity, speed);
     //Display in HTML here//
-
+    
     document.querySelector(".icon2").src = "https://openweathermap.org/img/wn/" + icon + ".png";
     document.querySelector(".temp2").innerText = "Temp:" + temp + "°F";
     document.querySelector(".wind2").innerText = "Wind:" + speed + "MPH";
@@ -107,7 +109,7 @@ fetch(fiveDayForecastURL)
      var { speed } = data.list[2].wind;
      console.log(icon, temp, humidity, speed);
      //Display in HTML here//
-
+     
      document.querySelector(".icon3").src = "https://openweathermap.org/img/wn/" + icon + ".png";
      document.querySelector(".temp3").innerText = "Temp:" + temp + "°F";
      document.querySelector(".wind3").innerText = "Wind:" + speed + "MPH";
@@ -122,7 +124,7 @@ fetch(fiveDayForecastURL)
      var { speed } = data.list[3].wind;
      console.log(icon, temp, humidity, speed);
      //Display in HTML here//
-
+     
      document.querySelector(".icon4").src = "https://openweathermap.org/img/wn/" + icon + ".png";
      document.querySelector(".temp4").innerText = "Temp:" + temp + "°F";
      document.querySelector(".wind4").innerText = "Wind:" + speed + "MPH";
@@ -137,35 +139,37 @@ fetch(fiveDayForecastURL)
      var { speed } = data.list[4].wind;
      console.log(icon, temp, humidity, speed);
      //Display in HTML here//
-
+     
      document.querySelector(".icon5").src = "https://openweathermap.org/img/wn/" + icon + ".png";
      document.querySelector(".temp5").innerText = "Temp:" + temp + "°F";
      document.querySelector(".wind5").innerText = "Wind:" + speed + "MPH";
      document.querySelector(".humidity5").innerText = "Humidity:" + humidity + "%";
      document.querySelector("#day-5").innerText = nextDay5;
 
-    })
-    .catch(function(error) {
-        console.log(error);
-    })
+})
+.catch(function(error) {
+    console.log(error);
+})
+
+    
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+//add event listener on click of search button
+searchButton.addEventListener("click", search);
+
+//Function for text input if it's a city or not
+function search() {
+    var cities = input.Value;
+    if (cities === "") {
+        console.log("error");
+        return;
+}};
+}
+
+getWeather();
 
 
 
 
-
-
-
-
-
-
-
-
-//GIVEN a weather dashboard with form inputs WHEN I search for a city
-//THEN I am presented with current and future conditions for that city and that city is added to the search history
-//WHEN I view current weather conditions for that city
-//THEN I am presented with the city name, the date, an icon representation of weather conditions, 
-//the temperature, the humidity, and the the wind speed
-//WHEN I view future weather conditions for that city
-//THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-//WHEN I click on a city in the search history
-//THEN I am again presented with current and future conditions for that city
