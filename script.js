e95657e50a58a4dfab0fdb9f295f605514969a1a
Fih <124883284+Firene11@users.noreply.github.com>
2023-06-26 16:55:32 -0300

    I need help

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 var APIKey = "6509d1e4a713732bea01a624be400633";
+var city = "Miami"
+var cityInput = document.querySelector("#input-box").value;
+city = cityInput.value;
+
+//Variables connected to DOM
+var searchButton = document.querySelector("#search-button");
+var todaysWeather = document.querySelector(".city-name");
+var searchOutput = document.getElementById("#search-history");
+//Stored cities array
+var searchedCities = [];
+
+function getWeather(city) {
 
 //https://api.openweathermap.org/data/2.5/weather?id={city id}&appid={API key}
 var weatherURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=6509d1e4a713732bea01a624be400633";
@@ -6,17 +18,7 @@ var weatherURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&
 // five day forecast -- api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 //var fiveDayForecastURL = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=6509d1e4a713732bea01a624be400633";
 
-//Variables connected to DOM
-var cityInput = document.querySelector("#input-box");
-var searchButton = document.querySelector("#search-button");
-var todaysWeather = document.querySelector(".city-name");
-var searchOutput = document.getElementById("#search-history");
 
-//Stored cities array
-var searchedCities = [];
-
-
-function getWeather(city) {
 
 //Fetch request for data from API
 fetch(weatherURL)
@@ -32,28 +34,32 @@ fetch(weatherURL)
 .then(function(data) {
     console.log(data);
     //pull data from object array
-    var cityName = data.name;
-
-    
-
-    //the [0] pulls from the first element of the array
+    var { name } = data;
     var date = dayjs().format ("dddd-MM-DD-YYYY");
     var { icon } = data.weather[0];
     var { temp, humidity } = data.main;
     var { speed } = data.wind;
     console.log(name, icon, temp, humidity, speed);
 
-    //Display in HTML here//
-    document.querySelector(".city-name").innerText = data.name;
-    document.querySelector(".icon-today").src = "https://openweathermap.org/img/wn/" + icon + ".png";
-    document.querySelector(".temp-today").innerText = "Temp:" + temp + "°F";
-    document.querySelector(".wind-today").innerText = "Wind:" + speed + "MPH";
-    document.querySelector(".humidity-today").innerText = "Humidity:" + humidity + "%";
-    document.querySelector(".date-today").innerText = date;
+        //Display in HTML here//
+        document.querySelector(".city-name").innerText = name;
+        document.querySelector(".icon-today").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".temp-today").innerText = "Temp:" + temp + "°F";
+        document.querySelector(".wind-today").innerText = "Wind:" + speed + "MPH";
+        document.querySelector(".humidity-today").innerText = "Humidity:" + humidity + "%";
+        document.querySelector(".date-today").innerText = date;
 })
 .catch(function(error) {
     console.log(error);
 })
 }
 
-getWeather(city);
+//event listener on search button
+searchButton.addEventListener("click", function() {
+    city = cityInput.value;
+    getWeather(city);
+})
+
+
+
+
