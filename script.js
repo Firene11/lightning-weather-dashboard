71697dcd93650d59efd5bfec1999f64b94b0c79e
Fih <124883284+Firene11@users.noreply.github.com>
2023-06-27 20:53:57 -0300

    fixed classnames in icons in 5 day forecast, now working

--- script.js
+++ script.js
@@ -56,8 +56,9 @@ fetch(weatherURL)
 })
 }
 
-function geoCode() {
+function geoCode(cityInput) {
 
+var cityInput = document.querySelector("#input-box").value;
 //geocode: http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
 var geoURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityInput + '&appid=' + APIKey + '&units=metric';
 
@@ -68,15 +69,15 @@ fetch(geoURL)
 .then(function (data) {
     var lat = data[0].lat;
     var lon = data[0].lon;
+    forecast(lat, lon);
 })
 }
 
 //five day forecast
-function forecast() {
-
-    var cityInput = document.querySelector("#input-box").value;
+function forecast(lat, lon) {
+    
     // five day forecast -- api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
-    var fiveDayForecastURL = 'http://api.openweathermap.org/data/2.5/forecast?q=' + cityInput + '&appid=' + APIKey + '&units=metric';
+    var fiveDayForecastURL = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + APIKey + '&units=metric';
 
     fetch(fiveDayForecastURL)
     .then(function (response) {
@@ -84,6 +85,7 @@ function forecast() {
     })
     .then(function (data) {
 
+        geoCode();
         //loop through data
         for (let i = 0; i < data.list.length; i += 5) {
 
@@ -91,6 +93,8 @@ function forecast() {
         var day1 = dayjs(date).add(1, "day");
         var day2 = dayjs(date).add(2, "day");
         var day3 = dayjs(date).add(3, "day");
+        var day4 = dayjs(date).add(4, "day");
+        var day5 = dayjs(date).add(5, "day");
         var icon = data.list[i].weather[0].icon;
         var temp = data.list[i].main.temp;
         var humidity = data.list[i].main.humidity;
@@ -98,25 +102,39 @@ function forecast() {
         console.log(temp, humidity, wind);
 
         //Display in HTML DAY ONE
-        document.querySelector(".icon-1").src = "https://openweathermap.org/img/wn/" + data.list[0].weather[0].icon + ".png";
-        document.querySelector(".temp1").innerText = "Temp:" + data.list[0].main.temp + "°F";
+        document.querySelector(".icon1").src = "https://openweathermap.org/img/wn/" + data.list[0].weather[0].icon + ".png";
+        document.querySelector(".temp1").innerText = "Temp:" + data.list[0].main.temp + "°C";
         document.querySelector(".wind1").innerText = "Wind:" + data.list[0].wind.speed + "MPH";
         document.querySelector(".humidity1").innerText = "Humidity:" + data.list[0].main.humidity + "%";
         document.querySelector("#date1").innerText = day1;
  
         //Display in HTML DAY TWO
-        //document.querySelector(".icon-2").src = "https://openweathermap.org/img/wn/" + data.list[1].weather[0].icon + ".png";
-        document.querySelector(".temp2").innerText = "Temp:" + data.list[1].main.temp + "°F";
+        document.querySelector(".icon2").src = "https://openweathermap.org/img/wn/" + data.list[1].weather[0].icon + ".png";
+        document.querySelector(".temp2").innerText = "Temp:" + data.list[1].main.temp + "°C";
         document.querySelector(".wind2").innerText = "Wind:" + data.list[1].wind.speed + "MPH";
         document.querySelector(".humidity2").innerText = "Humidity:" + data.list[1].main.humidity + "%";
         document.querySelector("#date2").innerText = day2;
 
         //Display in HTML DAY THREE
-        //document.querySelector(".icon-3").src = "https://openweathermap.org/img/wn/" + data.list[2].weather[0].icon + ".png";
-        document.querySelector(".temp3").innerText = "Temp:" + data.list[2].main.temp + "°F";
+        document.querySelector(".icon3").src = "https://openweathermap.org/img/wn/" + data.list[2].weather[0].icon + ".png";
+        document.querySelector(".temp3").innerText = "Temp:" + data.list[2].main.temp + "°C";
         document.querySelector(".wind3").innerText = "Wind:" + data.list[2].wind.speed + "MPH";
         document.querySelector(".humidity3").innerText = "Humidity:" + data.list[2].main.humidity + "%";
         document.querySelector("#date3").innerText = day3;
+
+        //Display in HTML DAY FOUR
+        document.querySelector(".icon4").src = "https://openweathermap.org/img/wn/" + data.list[3].weather[0].icon + ".png";
+        document.querySelector(".temp4").innerText = "Temp:" + data.list[3].main.temp + "°C";
+        document.querySelector(".wind4").innerText = "Wind:" + data.list[3].wind.speed + "MPH";
+        document.querySelector(".humidity4").innerText = "Humidity:" + data.list[3].main.humidity + "%";
+        document.querySelector("#date4").innerText = day4;
+
+        //Display in HTML DAY FIVE
+        document.querySelector(".icon5").src = "https://openweathermap.org/img/wn/" + data.list[4].weather[0].icon + ".png";
+        document.querySelector(".temp5").innerText = "Temp:" + data.list[4].main.temp + "°C";
+        document.querySelector(".wind5").innerText = "Wind:" + data.list[4].wind.speed + "MPH";
+        document.querySelector(".humidity5").innerText = "Humidity:" + data.list[4].main.humidity + "%";
+        document.querySelector("#date5").innerText = day5;
 
         }
     })
@@ -125,6 +143,7 @@ function forecast() {
 
 //event listener on search button
 searchButton.addEventListener("click", getWeather);
+searchButton.addEventListener("click", geoCode);
 searchButton.addEventListener('click', forecast);
 
 window.addEventListener("load", function () {
